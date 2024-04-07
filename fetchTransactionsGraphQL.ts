e572d0fcd23f470220d78cb61017afcd5010c7fa
jan-krivec <jan.krivec98@gmail.com>
2024-04-07 11:41:11 +0200

    query + blocktime

--- fetchTransactionsGraphQL.ts
+++ fetchTransactionsGraphQL.ts
@@ -1,65 +1,62 @@
-import { ApolloClient, InMemoryCache, gql, HttpLink } from '@apollo/client/core';
-import { fetch } from 'cross-fetch';
+import {ApolloClient, InMemoryCache, gql, HttpLink} from '@apollo/client/core';
+import axios from 'axios';
+import {fetch} from 'cross-fetch';
 
 // Polyfill fetch in the global scope
 global.fetch = fetch;
 
-// Set up Apollo Client
-const client = new ApolloClient({
-  link: new HttpLink({
-    uri: 'https://streaming.bitquery.io/graphql',
-    headers: {
-      'Content-Type': 'application/json',
-      'Authorization': 'Bearer ory_at_O9OKI6Ga4Uoas9z5TH5-L-PY8EHJWPuIMQsk4VHKf60.wFPNep4F9bUuvS7qmVx0pNhI9lRo8nABy1Ox47etE-E'
-    },
-    fetch,
-  }),
-  cache: new InMemoryCache(),
-});
+async function fetchTransactionsGraphQL() {
+  // Set up Apollo Client
+  const client = new ApolloClient({
+    link: new HttpLink({
+      uri: 'https://streaming.bitquery.io/graphql',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer token'
+      },
+      fetch,
+    }),
+    cache: new InMemoryCache(),
+  });
+
 
 // Define the GraphQL query
-const GET_TRADE_INFO = gql`
-query GetTradeInfo($network: evm_network, $token: String!, $token2: String!) {
-  EVM(network: $network, dataset: combined) {
-    Unique_Buyers: DEXTrades(
-      where: {Block: {Time: {since: "2023-09-27T01:00:00Z", till: "2023-09-27T02:00:00Z"}}, Trade: {Buy: {Currency: {SmartContract: {is: $token}}}, Sell: {Currency: {SmartContract: {is: $token2}}}}}
-    ) {
-      count(distinct: Trade_Buy_Buyer)
-    }
-    Unique_Sellers: DEXTrades(
-      where: {Block: {Time: {since: "2023-08-26T01:00:00Z", till: "2023-08-26T02:00:00Z"}}, Trade: {Sell: {Currency: {SmartContract: {is: $token}}}, Buy:{Currency:{SmartContract:{is: $token2}}}}}
-    ) {
-      count(distinct: Trade_Sell_Seller)
-    }
-    Total_Transactions: DEXTrades(
-      where: {Block: {Time: {since: "2023-09-27T01:00:00Z", till: "2023-09-27T02:00:00Z"}}, Trade: {Buy: {Currency: {SmartContract: {is: $token}}}, Sell: {Currency: {SmartContract: {is: $token2}}}}}
-    ) {
-      count(distinct: Transaction_Hash)
-    }
-    Total_Buy_Amount: DEXTrades(
-      where: {Block: {Time: {since: "2023-09-27T01:00:00Z", till: "2023-09-27T02:00:00Z"}}, Trade: {Buy: {Currency: {SmartContract: {is: $token}}}, Sell: {Currency: {SmartContract: {is: $token2}}}}}
-    ) {
-      sum(of:Trade_Buy_Amount)
-    }
-    Total_Sell_Amount: DEXTrades(
-      where: {Block: {Time: {since: "2023-09-27T01:00:00Z", till: "2023-09-27T02:00:00Z"}}, Trade: {Buy: {Currency: {SmartContract: {is: $token}}}, Sell: {Currency: {SmartContract: {is: $token2}}}}}
-    ) {
-      sum(of:Trade_Sell_Amount)
-    }
+  const GET_TRADE_INFO = gql`
+    query MyQuery {
+      EVM(network: eth, $token: String!, $startblock: Number!, $endblock: Number!) {
+        DEXTrades(where: {Block: {Number: {lt: "$startblock", gt: "$endblock"}}}) {
+          Trade {
+            Buy {
+              Currency {
+                SmartContract(selectWhere: {is: "$token"})
+              }
+            }
+          }
+        }
+      }
+    }`;
+
+  const blockTimeRes = await axios.get("https://coins.llama.fi/block/ethereum/1677274655");
+  let blockEnd = 0;
+  let blockStart = 0;
+  if (blockTimeRes.status == 200) {
+    blockEnd  = blockTimeRes.data.height;
+    blockStart = blockEnd - 300; // prb ura manj
   }
-}`;
 
 // Define variables for the query
-const variables = {
-  network: "eth",
-  token: "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2",
-  token2: "0x20561172f791f915323241e885b4f7d5187c36e1"
-};
+  const variables = {
+    network: "eth",
+    token: "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2",
+    startblock: blockStart,
+    endblock: blockEnd
+  };
 
 // Execute the query
-client.query({
-  query: GET_TRADE_INFO,
-  variables: variables,
-})
-  .then(result => console.log("Complete Data:", JSON.stringify(result.data, null, 2)))
-  .catch(error => console.error(error));
+  client.query({
+    query: GET_TRADE_INFO,
+    variables: variables,
+  })
+      .then(result => console.log("Complete Data:", JSON.stringify(result.data, null, 2)))
+      .catch(error => console.error(error));
+}
